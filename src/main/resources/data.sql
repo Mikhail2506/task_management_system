e242dec5591c98d99e8e56befd9dac5a4e4dcd31
mikhail_toukach <m.toukach@astondevs.ru>
2025-01-27 11:29:14 +0300

    тесты готовы, Docker, Readme

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 INSERT INTO public.users (user_name, user_email, user_password, role)
-VALUES ('Author', 'author@mail.ru', '$2a$10$5v5Zz5z5z5z5z5z5z5z5u', 'ADMIN');
+SELECT 'Author', 'author@mail.ru', '$2a$10$5v5Zz5z5z5z5z5z5z5z5u', 'ADMIN'
+WHERE NOT EXISTS (
+    SELECT 1 FROM public.users WHERE user_email = 'author@mail.ru'
+);
